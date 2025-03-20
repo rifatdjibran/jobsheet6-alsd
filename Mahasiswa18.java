c46b97c1d82fd112a3b8290fdc44a4f7931fa955
rifat djibran <rifatdjibran099@gmail.com>
2025-03-20 13:46:17 +0700

    Praktikum 2 & 3 & 4

--- Mahasiswa18.java
+++ Mahasiswa18.java
@@ -19,6 +19,7 @@ public class Mahasiswa18 {
         System.out.println("NIM   : " + nim);
         System.out.println("Kelas : " + kelas);
         System.out.println("IPK   : " + ipk);
-        System.out.println("----------------------------");
+        System.out.println("-----------------------------");
     }
 }
+
